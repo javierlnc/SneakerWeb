36f84b86a2e2915cf4f4f8c03750cb79e3fd979e
javierlnc <javierlnc@icloud.com>
2025-01-03 21:01:36 -0500

    create app and serializers

---
+++ backend_api/sneakers_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
