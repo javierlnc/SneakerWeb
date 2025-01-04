b3b0f4c5a362b0d427abeda84cf927840b368f7a
javierlnc <javierlnc@icloud.com>
2025-01-03 21:05:38 -0500

    update UsersSerializer to specify fields and set read-only fields

--- backend_api/sneakers_app/serializers.py
+++ backend_api/sneakers_app/serializers.py
@@ -4,4 +4,5 @@ from .models import Users
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = '__all__'
+        fields = ( 'username', 'first_name', 'last_name', 'email', 'password', 'created_at', 'updated_at')
+        reads_only_fields = ('created_at', 'updated_at')
